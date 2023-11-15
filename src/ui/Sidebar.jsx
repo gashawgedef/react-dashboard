98d6a8f610c1f85b3d63698dc2674472272e2861
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-11-15 15:37:09 -0800

    adding the three components

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -3,7 +3,7 @@ import MainNav from "./MainNav"
 import Logo from '../Components/Logo'
 
 const StyledSidebar = styled.aside`
-  background-color: var(--color-grey-0);
+  background-color:#F8F8FF;
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
 
