6332dbcc99a82eed262de5661a09862d9831ea73
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-11-15 14:40:40 -0800

    gashaw front en d supabase postgress database

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://zrysxkndwkurtkkmvwaq.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyeXN4a25kd2t1cnRra212d2FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAwNDYyOTYsImV4cCI6MjAxNTYyMjI5Nn0.7DpWbattAaoiCzzzlMx5-K76zVa43jU_ojPsYRi9w1U'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
+
