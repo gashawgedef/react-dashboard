548d415e10c6f39ab1e9dd5e84b9db6f0e3c6728
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-11-16 14:47:15 -0800

    reacts auery ffor cabins crud operation

--- src/Pages/Cabins/CreateCabinForm.jsx
+++ src/Pages/Cabins/CreateCabinForm.jsx
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 
-import Input from "../../ui/Input";
+import Input from "../../ui/Input";    
 import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
-// import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { createCabins } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+import {useMutation, useQueryClient } from "@tanstack/react-query";
 
 const FormRow = styled.div`
   display: grid;
@@ -44,37 +47,82 @@ const Label = styled.label`
 // `;
 
 function CreateCabinForm() {
-  // const[register,handleSubmint]=useForm();
+  const { handleSubmit, register, errors ,reset} = useForm();
+  const queryClient=useQueryClient();
+  const {mutate,isLoading:isCreating}=useMutation({
+    mutationFn:createCabins,
+    onSuccess:()=>{
+      toast.success("New Cabin Created Successfully")
+      queryClient.invalidateQueries({
+        queryKey:['cabins']});
+        reset();
+    },
+    onError:(err)=>toast.error(err.message)
+    
+    
+
+});
+
+ function gashity(){
+  console.log(errors)
+ }
+ gashity();
+  function onSubmits(data){
+    mutate(data);
+    console.log(data)
+
+  }  
+
+  function onError(errors){
+    console.log(errors)
+
+  }
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmits,onError)}>
       <FormRow>
         <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" />
+        <Input type="text" id="name" {...register("name",{
+          required:"This Field is Required"
+        })}/>
       </FormRow>
 
       <FormRow>
         <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" />
+        <Input type="number" id="maxCapacity"  {...register("maxCapacity",{
+          required:"This Field is Required"
+        })}/>
       </FormRow>
 
       <FormRow>
         <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" />
+        <Input type="number" id="regularPrice"  {...register("regularPrice",{
+          required:"This Field is Required"
+        })}/>
       </FormRow>
 
       <FormRow>
         <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} />
+        <Input type="number" id="discount" defaultValue={0} {...register("discount",{
+          required:"This Field is Required"
+        })} />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" />
+        <Textarea type="number" id="description" defaultValue="" {...register("description",{
+          required:"This Field is Required"
+        })}/>
       </FormRow>
 
       <FormRow>
         <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", {
+            required:   "This field is required",
+          })}
+        />
       </FormRow>
 
       <FormRow>
@@ -82,7 +130,7 @@ function CreateCabinForm() {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button>Edit cabin</Button>
+        <Button disabled={isCreating}>Edit cabin</Button>
       </FormRow>
     </Form>
   );
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,7 +1,7 @@
+
 import supabase from "./supabase";
 
 export async function getCabins() {
-
     const { data, error } = await supabase
         .from('cabins')
         .select('*');
@@ -27,5 +27,23 @@ export async function deleteCabins(id){
     }
 
     return data
+  
+}
+
+export async function createCabins(newCabin){
+
+    const { data, error } = await supabase
+    .from('cabins')
+    .insert([newCabin ])
+    .select();
+
+    if (error) {
+        console.error(error)
+        throw new Error(`Cabins could not be created }`)
+
+    }
+    return data
+  
+
   
 }
